db99d78f113b1af9f66d42f84336a21c60a02626
Lee-KyungSeok <leeseok8347@gmail.com>
2024-04-11 22:06:24 +0900

    Add 2758 solution by cory

--- medium/02759-RequiredByKeys/cory.ts
+++ medium/02759-RequiredByKeys/cory.ts
@@ -1,9 +1,9 @@
 export type Intersection<T> = {
-    [P in keyof T]: T[P]
+    [P in keyof T]: T[P];
 }
 
-export type PartialByKeys<T, K extends keyof T = keyof T> = Intersection<{
-    [P in keyof T as P extends K ? never : P]: T[P]
+export type RequiredByKeys<T extends object, K extends keyof T = keyof T> = Intersection<{
+    [P in keyof T as P extends K ? P : never]-?: T[P]
 } & {
-    [P in keyof T as P extends K ? P : never]?: T[P]
+    [P in keyof T as P extends K ? never : P]: T[P]
 }>
