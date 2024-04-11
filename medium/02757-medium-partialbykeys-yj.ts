6ac8e294d201b481cf6e32503be054f838ee759e
YeaJeong <bananana0118@gmail.com>
2024-04-11 21:42:51 +0900

    02757-medium-partialbykeys

---
+++ medium/02757-medium-partialbykeys-yj.ts
@@ -0,0 +1,9 @@
+
+type InterSectionType<T> = {[key in keyof T] : T[key]}
+
+type PartialByKeys<T, K extends keyof T = keyof T> = InterSectionType<
+{[key in keyof T as key extends K ? key : never]? :T[key]}
+&
+{
+  [key in keyof T as key extends K ? never : key]:T[key]
+}>
