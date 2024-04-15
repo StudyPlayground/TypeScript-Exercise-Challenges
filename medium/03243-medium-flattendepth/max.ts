8d0ed098048f73a54fd18d1dec7c906e26d80075
pirateTurtle <75886763+wogha95@users.noreply.github.com>
2024-04-15 15:14:49 +0900

    03243-medium-flattendepth

---
+++ medium/03243-medium-flattendepth/max.ts
@@ -0,0 +1,13 @@
+// solve with solution
+
+type FlattenDepth<
+  T extends any[],
+  S extends number = 1,
+  U extends any[] = []
+> = U['length'] extends S
+  ? T
+  : T extends [infer F, ...infer R]
+    ? F extends any[]
+      ? [...FlattenDepth<F, S, [...U, 1]>, ...FlattenDepth<R, S, U>]
+      : [F, ...FlattenDepth<R, S, U>]
+    : T;
