fd69238422f1a13b9b2b93fd8b235b4f37c7a6c4
pirateTurtle <75886763+wogha95@users.noreply.github.com>
2024-04-15 22:30:38 +0900

    03196-medium-flip-arguments

--- medium/03196-medium-flip-arguments/max.ts
+++ medium/03196-medium-flip-arguments/max.ts
@@ -1,3 +1,3 @@
-type Reverse<T extends any[]> = T extends [infer A, ...infer Rest] ? [...Reverse<Rest>, A] : [];
+export type Reverse<T extends any[]> = T extends [infer A, ...infer Rest] ? [...Reverse<Rest>, A] : [];
 
-type FlipArguments<T extends (...args: any[]) => any> = T extends (...args: infer Args) => infer R ? (...args: Reverse<Args>) => R : never;
+export type FlipArguments<T extends (...args: any[]) => any> = T extends (...args: infer Args) => infer R ? (...args: Reverse<Args>) => R : never;
