c48115764986354875467602f4d4be4e14a68204
YeaJeong <bananana0118@gmail.com>
2024-04-15 17:40:34 +0900

    Revert "02946-medium-objectentries"

--- medium/02946-medium-objectentries/max.ts
+++
@@ -1,3 +0,0 @@
-type ObjectEntries<T, U = Required<T>> = {
-    [key in keyof U]: [key, U[key] extends never ? undefined : U[key]]
-}[keyof U];
--- medium/02946-medium-objectentries/yj.ts
+++
@@ -1,3 +0,0 @@
-export type ObjectEntries<T, U= Required<T>> = {
-    [key in keyof U]:[key, U[key] extends never? undefined:U[key]]
-  }[keyof U]
