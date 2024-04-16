e12c35f72c896df25d45bd1aaa0062b84971d802
Kyungseok Lee <leeseok8347@gmail.com>
2024-04-16 21:07:52 +0900

    03326-medium-bem-style-string

--- medium/03326-medium-bem-style-string/cory.ts
+++ medium/03326-medium-bem-style-string/cory.ts
@@ -1,1 +1,3 @@
-// 답을 적어주세요.
+export type SplitElement<T extends readonly any[]> = T['length'] extends 0 ? '' : `__${T[number]}`;
+export type SplitModifier<T extends readonly any[]> = T['length'] extends 0 ? '' : `--${T[number]}`;
+export type BEM<B extends string, E extends string[], M extends string[]> = `${B}${SplitElement<E>}${SplitModifier<M>}`;
