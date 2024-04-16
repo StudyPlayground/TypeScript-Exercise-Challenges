78a6353d13dead0724e4aff29dfae4da3447d0cd
Kyungseok Lee <leeseok8347@gmail.com>
2024-04-16 20:54:23 +0900

    03243-medium-flattendepth

--- medium/03243-medium-flattendepth/cory.ts
+++ medium/03243-medium-flattendepth/cory.ts
@@ -1,1 +1,17 @@
-// 답을 적어주세요.
+/* 아래처럼 하면 infinity 에러남 ㅜㅜ
+export type Flatten<T extends any[]> = T extends [[...infer F], ...infer R] ? [...F, ...Flatten<R>]
+    : T extends [infer F, ...infer R] ? [F, ...Flatten<R>]
+    : T
+
+export type FlattenDepth<T extends any[], R extends number = 1, U extends any[] = []> = U['length'] extends R ? T
+    : FlattenDepth<Flatten<T>, R, [...U, 1]>
+ */
+
+    export type FlattenDepth<
+    T extends any[],
+    S extends number = 1,
+    U extends any[] = []
+> = U['length'] extends S ? T
+    : T extends [[...infer F], ...infer R] ? [...FlattenDepth<F, S, [...U, 1]>, ...FlattenDepth<R, S, U>]
+        : T extends [infer F, ...infer R] ? [F, ...FlattenDepth<R, S, U>]
+            : T
