79481e01a47c1cc36b64ccf711104bbbb2918d93
Kyungseok Lee <leeseok8347@gmail.com>
2024-04-16 11:02:02 +0000

    03196-medium-flip-arguments

--- medium/03196-medium-flip-arguments/cory.ts
+++ medium/03196-medium-flip-arguments/cory.ts
@@ -1,1 +1,5 @@
-// 답을 적어주세요.
+export type Reverse<T extends any[]> = T extends [infer F, ...infer R] ? [...Reverse<R>, F] : [];
+
+export type FlipArguments<T extends (...args: any[]) => any> = T extends (...args: infer Args) => infer Return
+    ? (...args: Reverse<Args>) => Return
+    : never;
