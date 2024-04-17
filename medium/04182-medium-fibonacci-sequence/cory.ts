25bc04f249dd69a96636e4b9f485a2b85aea6606
Kyungseok Lee <leeseok8347@gmail.com>
2024-04-17 23:20:26 +0900

    04182-medium-fibonacci-sequence

--- medium/04182-medium-fibonacci-sequence/cory.ts
+++ medium/04182-medium-fibonacci-sequence/cory.ts
@@ -1,1 +1,3 @@
-// 답을 적어주세요.
+export type Fibonacci<T extends number, S extends any[] = [0], Prev extends any[] = [], Curr extends any[] = [0]> = S['length'] extends T
+    ? Curr['length']
+    : Fibonacci<T, [0, ...S], Curr, [...Prev, ...Curr]>
