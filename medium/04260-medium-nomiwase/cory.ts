5dbc92112abe7ab1ebb7d6888d680393200fb888
Kyungseok Lee <leeseok8347@gmail.com>
2024-04-18 20:08:47 +0900

    04260-medium-nomiwase

--- medium/04260-medium-nomiwase/cory.ts
+++ medium/04260-medium-nomiwase/cory.ts
@@ -1,1 +1,8 @@
-// 답을 적어주세요.
+export type Combination<T extends string> = [T] extends [never] ? ''
+    : '' | {[K in T]: `${K}${Combination<Exclude<T, K>>}`}[T];
+
+export type SplitToUnion<S extends string> = S extends `${infer A}${infer B}`
+    ? A | SplitToUnion<B>
+    : S;
+
+export type AllCombinations<S extends string> = Combination<SplitToUnion<S>>
