c415f72602c7e1da9009b4e02147fde2291d6f75
Kyungseok Lee <leeseok8347@gmail.com>
2024-04-20 02:21:43 +0900

    04499-medium-chunk

--- medium/04499-medium-chunk/cory.ts
+++ medium/04499-medium-chunk/cory.ts
@@ -1,1 +1,6 @@
-// 답을 적어주세요.
+export type Chunk<T extends any[], C extends number, Result extends any[] = []> = Result['length'] extends C ? [Result, ...Chunk<T, C>]
+    : T extends [infer U, ...infer R]
+        ? Chunk<R, C, [...Result, U]>
+    : Result extends []
+        ? Result
+    : [Result]
