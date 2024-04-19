758d7cbd0984ac0357db1ec900c32250f7786f6b
Kyungseok Lee <leeseok8347@gmail.com>
2024-04-20 02:50:25 +0900

    04518-medium-fill

--- medium/04518-medium-fill/cory.ts
+++ medium/04518-medium-fill/cory.ts
@@ -1,1 +1,14 @@
-// 답을 적어주세요.
+export type Fill<
+    T extends unknown[],
+    N,
+    Start extends number = 0,
+    End extends number = T['length'],
+    withFill extends boolean = Start extends 0 ? true : false,
+    Result extends unknown[] = [],
+> = T extends [infer F, ...infer R]
+        ? Result['length'] extends End
+            ? Fill<R, N, Start, End, false, [...Result, F]>
+        : Result['length'] extends Start
+            ? Fill<R, N, Start, End, true, [...Result, N]>
+        : Fill<R, N, Start, End, withFill, [...Result, withFill extends true ? N : F]>
+    : Result
