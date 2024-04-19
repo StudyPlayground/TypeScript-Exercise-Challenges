471c418638a1459aab66a45f3597b910c5b7abb4
Kyungseok Lee <leeseok8347@gmail.com>
2024-04-19 21:24:39 +0900

    04471-medium-zip

--- medium/04471-medium-zip/cory.ts
+++ medium/04471-medium-zip/cory.ts
@@ -1,1 +1,5 @@
-// 답을 적어주세요.
+export type Zip<T extends any[], U extends any[], Result extends any[] = []> = T extends [infer TF, ...infer TR]
+    ? U extends [infer UF, ...infer UR]
+        ? Zip<TR, UR, [...Result, [TF, UF]]>
+        : Result
+    : Result;
