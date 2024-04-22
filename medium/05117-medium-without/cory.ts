aa3112962d77e657ab5a6eb20ca4bfa3ce302f53
Kyungseok Lee <leeseok8347@gmail.com>
2024-04-22 20:02:20 +0900

    05117-medium-without

--- medium/05117-medium-without/cory.ts
+++ medium/05117-medium-without/cory.ts
@@ -1,1 +1,16 @@
-// 답을 적어주세요.
+export type IsEqual<X, Y> = (<T>() => T extends X ? 1 : 2) extends (<T>() => T extends Y ? 1 : 2) ? true : false;
+export type Includes<T extends any[], U> =  T extends [infer F, ...infer R]
+    ? IsEqual<F, U> extends true ? true
+    : Includes<R, U>
+    : false
+
+export type Without<
+    T extends any[],
+    U,
+    Result extends any[] = [],
+    K extends any[] = U extends any[] ? U : [U],
+> = T extends [infer F, ...infer R]
+    ? Includes<K, F> extends true
+        ? Without<R, U, Result, K>
+        : Without<R, U, [...Result, F], K>
+    : Result;
