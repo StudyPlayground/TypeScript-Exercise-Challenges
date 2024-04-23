f8d785678a931ea41532ad380b73158e30d327c3
Kyungseok Lee <leeseok8347@gmail.com>
2024-04-23 20:14:30 +0900

    05153-medium-indexof

--- medium/05153-medium-indexof/cory.ts
+++ medium/05153-medium-indexof/cory.ts
@@ -1,1 +1,6 @@
-// 답을 적어주세요.
+export type IsEqual<X, Y> = (<T>() => T extends X ? 1 : 2) extends (<T>() => T extends Y ? 1 : 2) ? true : false;
+export type IndexOf<T extends any[], U, Result extends any[] = []> = T extends [infer F, ...infer R]
+    ? IsEqual<F, U> extends true
+        ? Result['length']
+        : IndexOf<R, U, [...Result, 0]>
+    : -1;
