b4fba1732c751ae4ea7890edc3e690f0821e487b
Kyungseok Lee <leeseok8347@gmail.com>
2024-04-25 18:41:00 +0900

    05317-medium-lastindexof

--- medium/05317-medium-lastindexof/cory.ts
+++ medium/05317-medium-lastindexof/cory.ts
@@ -1,1 +1,5 @@
-// 답을 적어주세요.
+export type LastIndexOf<T extends any[], U> = T extends [...infer F, infer R]
+    ? IsEqual<R, U> extends true
+        ? F['length']
+        : LastIndexOf<F, U> 
+    : -1;
