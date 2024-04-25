ffcf0e5f591c88d79dfd600f4ef5c65686452e89
Kyungseok Lee <leeseok8347@gmail.com>
2024-04-25 18:28:12 +0900

    05310-medium-join

--- medium/05310-medium-join/cory.ts
+++ medium/05310-medium-join/cory.ts
@@ -1,1 +1,5 @@
-// 답을 적어주세요.
+export type Join<T extends any[], U extends string | number> = T extends [infer F extends string, ...infer R]
+    ? R extends []
+        ? `${F}`
+        : `${F}${U}${Join<R, U>}`
+    : ''
