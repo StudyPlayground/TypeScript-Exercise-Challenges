0c092a55bff50d2168a4487ec42a02076d7bf0b8
Kyungseok Lee <leeseok8347@gmail.com>
2024-04-26 13:52:42 +0900

    05821-medium-maptypes

--- medium/05821-medium-maptypes/cory.ts
+++ medium/05821-medium-maptypes/cory.ts
@@ -1,1 +1,11 @@
-// 답을 적어주세요.
+export type MapFromTo<From extends any, To extends  any> = { mapFrom: From, mapTo: To };
+
+export type Transform<T, R extends MapFromTo<any, any>> = T extends R['mapFrom']
+    ? R extends infer U extends MapFromTo<any, any> 
+        ? U['mapFrom'] extends T ? U['mapTo'] : never 
+        : never
+    : T
+
+export type MapTypes<T extends object, R extends MapFromTo<any, any>> = {
+    [P in keyof T]: Transform<T[P], R>;
+}
