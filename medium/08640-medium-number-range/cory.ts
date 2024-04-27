7ca128cefaa72adeea06536547e9267e31a974e2
Kyungseok Lee <leeseok8347@gmail.com>
2024-04-27 23:24:47 +0900

    08640-medium-number-range

--- medium/08640-medium-number-range/cory.ts
+++ medium/08640-medium-number-range/cory.ts
@@ -1,1 +1,12 @@
-// 답을 적어주세요.
+export type LengthArray<L extends number, Result extends any[] = []> = Result['length'] extends L
+    ? Result
+    : LengthArray<L,  [...Result, 0]>;
+
+export type NumberRange<
+    L extends number,
+    H extends number,
+    Arr extends any[] = LengthArray<L>,
+    Result extends number = L,
+> = Arr['length'] extends H
+    ? Result | H
+    : NumberRange<L, H, [...Arr, 0], Result | Arr['length']>
