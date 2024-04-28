a7fea8e7958415541eaf8b067c3b9dd941da0970
Kyungseok Lee <leeseok8347@gmail.com>
2024-04-28 11:07:40 +0900

    08767-medium-combination

--- medium/08767-medium-combination/cory.ts
+++ medium/08767-medium-combination/cory.ts
@@ -1,1 +1,5 @@
-// 답을 적어주세요.
+export type CombUnion<T extends string, R extends string= T> = T extends infer U extends string
+    ? U | `${U} ${CombUnion<Exclude<R, U>>}`
+    : never;
+
+export type Combination<T extends string[]> = CombUnion<T[number]>;
