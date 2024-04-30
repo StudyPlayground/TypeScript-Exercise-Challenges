6a5378852939bb58ce97b25cbd64b2c850755b7a
Kyungseok Lee <leeseok8347@gmail.com>
2024-04-30 23:07:53 +0900

    09896-medium-get-middle-element

--- medium/09896-medium-get-middle-element/cory.ts
+++ medium/09896-medium-get-middle-element/cory.ts
@@ -1,1 +1,4 @@
-// 답을 적어주세요.
+export type Pop<T extends any[]> = T extends [...infer U, unknown] ? U : T;
+export type Shift<T extends [...any]> = T extends [unknown, ...infer B] ? B : [];
+
+export type GetMiddleElement<T extends any[]> = T['length'] extends 0 | 1 | 2 ? T : GetMiddleElement<Shift<Pop<T>>>
