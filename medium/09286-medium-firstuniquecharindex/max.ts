14ef34e794220226243d68d083f33b322640e04c
pirateTurtle <75886763+wogha95@users.noreply.github.com>
2024-04-30 18:05:18 +0900

    09286-medium-firstuniquecharindex

--- medium/09286-medium-firstuniquecharindex/max.ts
+++ medium/09286-medium-firstuniquecharindex/max.ts
@@ -1,5 +1,1 @@
-export type ParseUrlParams<T extends string> = T extends `${string}:${infer Rest}`
-  ? Rest extends `${infer A}/${infer B}`
-    ? A | ParseUrlParams<B>
-    : Rest
-  : never;
+// write your code
