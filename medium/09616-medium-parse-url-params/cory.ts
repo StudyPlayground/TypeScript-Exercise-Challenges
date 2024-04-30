d5018bee867e588e0f972f33c57b5d4f61356da3
Kyungseok Lee <leeseok8347@gmail.com>
2024-04-30 22:56:24 +0900

    09616-medium-parse-url-params

--- medium/09616-medium-parse-url-params/cory.ts
+++ medium/09616-medium-parse-url-params/cory.ts
@@ -1,1 +1,5 @@
-// 답을 적어주세요.
+export type ParseUrlParams<T extends string> = T extends `${string}:${infer R}` 
+    ? R extends `${infer F}/${infer K}` 
+        ? F | ParseUrlParams<K> 
+        : R
+    : never;
