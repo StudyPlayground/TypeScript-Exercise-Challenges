f91b8490921b7bd606aca0740116b369106bfbde
Kyungseok Lee <leeseok8347@gmail.com>
2024-05-02 20:28:57 +0900

    16259-medium-to-primitive

--- medium/16259-medium-to-primitive/cory.ts
+++ medium/16259-medium-to-primitive/cory.ts
@@ -1,1 +1,5 @@
-// 답을 적어주세요.
+export type toPrimitiveUnit<T> = T extends { valueOf: () => infer P } ? P : T;
+export type ToPrimitive<T> = T extends (...args: any[]) => any ? Function 
+    : T extends object 
+        ? { [K in keyof T]: ToPrimitive<T[K]> } 
+        : toPrimitiveUnit<T>
