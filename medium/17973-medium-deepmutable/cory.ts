3cc8ed5a66457ffb47e18149b1941894e513e788
Kyungseok Lee <leeseok8347@gmail.com>
2024-05-04 22:51:52 +0900

    17973-medium-deepmutable

--- medium/17973-medium-deepmutable/cory.ts
+++ medium/17973-medium-deepmutable/cory.ts
@@ -1,1 +1,10 @@
-// 답을 적어주세요.
+export type DeepMutableArr<T extends readonly any[], K = T extends readonly [...infer U] ? U : T> = K extends [infer F, ...infer R] 
+    ? [F extends object ? DeepMutable<F> : F, ...DeepMutableArr<R>] 
+    : K
+
+export type DeepMutable<T extends object> = T extends (...args: any) => any ? T : {
+    -readonly [K in keyof T]: T[K] extends any[] ? DeepMutableArr<T[K]> 
+        : T[K] extends Record<any, any> 
+            ? DeepMutable<T[K]> 
+            : T[K]
+}
