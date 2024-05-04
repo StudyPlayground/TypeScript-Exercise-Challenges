7e913e1e5962228d8c41de73d3ce66730126516c
Kyungseok Lee <leeseok8347@gmail.com>
2024-05-04 22:58:51 +0900

    18142-medium-all

--- medium/18142-medium-all/cory.ts
+++ medium/18142-medium-all/cory.ts
@@ -1,1 +1,2 @@
-// 답을 적어주세요.
+export type IsEqual<X, Y> = (<T>() => T extends X ? 1 : 0) extends (<T>() => T extends Y ? 1 : 0) ? true : false; 
+export type All<T extends any[], P> = T extends [infer F, ...infer R] ? IsEqual<F, P> extends true ? All<R, P> : false : true
