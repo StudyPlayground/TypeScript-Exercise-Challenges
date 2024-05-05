dc8dd8dd62c365ab798d11b6ad33162ca0ea5c84
Kyungseok Lee <leeseok8347@gmail.com>
2024-05-05 12:09:28 +0900

    21104-medium-findall

--- medium/21104-medium-findall/cory.ts
+++ medium/21104-medium-findall/cory.ts
@@ -1,1 +1,11 @@
-// 답을 적어주세요.
+export type StringArr<T extends string, Result extends string[] = []> = T extends `${infer F}${infer R}` ? StringArr<R,  [...Result, F]> : Result;
+
+export type FindAll<T extends string, P extends string, AddedChar extends string[] = []> = T extends '' ? [] : P extends '' ? []
+    : T extends `${infer F}${P}${infer R}`
+        ? [
+            [...AddedChar, ...StringArr<F>]['length'],
+            ...P extends `${infer PF}${infer PR}`
+                ? FindAll<`${PR}${R}`, P, [...AddedChar, ...StringArr<F>, PF]>
+                : []
+        ]
+        : [];
