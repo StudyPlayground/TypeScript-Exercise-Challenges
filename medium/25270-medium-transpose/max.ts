596584017fb6b007234e15b4a7a7cb6c0c69706a
pirateTurtle <75886763+wogha95@users.noreply.github.com>
2024-05-07 21:05:40 +0900

    25270-medium-transpose

---
+++ medium/25270-medium-transpose/max.ts
@@ -0,0 +1,7 @@
+export type Transpose<M extends number[][], R = M['length'] extends 0 ? [] : M[0]> = {
+  [X in keyof R]: {
+    [Y in keyof M]: X extends keyof M[Y]
+      ? M[Y][X]
+      : never
+  }
+}
