cce372f205e01ffac82448366533dce472dbd691
Kyungseok Lee <leeseok8347@gmail.com>
2024-05-07 20:22:31 +0900

    25270-medium-transpose

--- medium/25270-medium-transpose/cory.ts
+++ medium/25270-medium-transpose/cory.ts
@@ -1,1 +1,13 @@
-// 답을 적어주세요.
+export type ExtractIdx<
+    M extends number[][],
+    Idx extends any[] = []
+> = M extends [infer F extends number[], ...infer R extends number[][]]
+    ? [F[Idx['length']], ...ExtractIdx<R, Idx>]
+    : M;
+
+export type Transpose<
+    M extends number[][],
+    Result extends any[] = [],
+> = M extends [] ? [] : M[0]['length'] extends Result['length']
+    ? Result
+    : Transpose<M, [...Result, ExtractIdx<M, Result>]>
