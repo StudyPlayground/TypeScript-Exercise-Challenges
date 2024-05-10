c77b0dbb02c965ffd30ad5a95095a797901e08ee
Kyungseok Lee <leeseok8347@gmail.com>
2024-05-10 20:15:37 +0900

    27932-medium-mergeall

--- medium/27932-medium-mergeall/cory.ts
+++ medium/27932-medium-mergeall/cory.ts
@@ -1,1 +1,11 @@
-// 답을 적어주세요.
+export type Merge<T extends object, U extends object> = {
+    [K in (keyof T | keyof U)]: K extends keyof T 
+        ? K extends keyof U ? T[K] | U[K] : T[K]
+        : K extends keyof U ? U[K] : never
+}
+
+export type MergeAll<XS extends unknown[]> = XS extends [infer F, ...infer U] 
+    ? F extends any[] 
+        ? MergeAll<F> & MergeAll<U> 
+        : Merge<{ [K in keyof F]: F[K] }, MergeAll<U>>
+    : {}; 
