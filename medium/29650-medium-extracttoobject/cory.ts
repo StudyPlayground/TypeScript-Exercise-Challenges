8f59888c73239864d8e8736fc32f5962196a74e9
Kyungseok Lee <leeseok8347@gmail.com>
2024-05-12 04:56:11 +0900

    29650-medium-extracttoobject

--- medium/29650-medium-extracttoobject/cory.ts
+++ medium/29650-medium-extracttoobject/cory.ts
@@ -1,1 +1,7 @@
-// 답을 적어주세요.
+export type Intersection<T> = {
+    [K in keyof T]: T[K]
+}
+
+export type ExtractToObject<T extends object, U extends keyof T> = Intersection<
+    { [K in keyof T as K extends U ? never : K]: T[K] } & T[U]
+>
