31ff1f491cedc982ec5aaa5ffda84f34f61138a9
YeaJeong <bananana0118@gmail.com>
2024-05-12 18:15:15 +0900

    Revert "29785-medium-deep-omit"

--- medium/29785-medium-deep-omit/max.ts
+++
@@ -1,7 +0,0 @@
-export type DeepOmit<T, S extends string> = S extends `${infer A}.${infer Rest}`
-  ? {
-    [key in keyof T]: key extends A
-      ? DeepOmit<T[key], Rest>
-      : T[key]
-  }
-  : Omit<T, S>
--- medium/29785-medium-deep-omit/yj.ts
+++
@@ -1,5 +0,0 @@
-
-export type DeepOmit<O, P extends string> = P extends `${infer K}.${infer Rest}` ?{
-    [key in keyof O]: key extends K? DeepOmit<O[key],Rest> : O[key]
-  } : Omit<O,P>
-  
