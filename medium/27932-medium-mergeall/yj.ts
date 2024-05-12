fde97573a029a43217568acf9caf65cb6f3e3c46
YeaJeong <bananana0118@gmail.com>
2024-05-12 15:09:59 +0900

    27932-medium-mergeall

--- medium/27932-medium-mergeall/yj.ts
+++ medium/27932-medium-mergeall/yj.ts
@@ -1,1 +1,7 @@
-//dasdasd
+export type MergeAll<
+XS extends object[], 
+U = XS[number] , //키가 PropertyKey형태로 U가 스스로의 U일때 키를 뽑고 아니면 안함
+keys extends PropertyKey = U extends U ? keyof U : never> = 
+{
+  [K in keys] : U extends U ? U [K & keyof U] : never
+}
