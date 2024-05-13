d336fbd6db85a85ce86f5e541df8445e8a799bf5
Kyungseok Lee <leeseok8347@gmail.com>
2024-05-13 21:26:21 +0900

    30430-medium-tower-of-hanoi

--- medium/30430-medium-tower-of-hanoi/cory.ts
+++ medium/30430-medium-tower-of-hanoi/cory.ts
@@ -1,1 +1,13 @@
-// 답을 적어주세요.
+export type Hanoi<N extends number, From = 'A', To = 'B', Intermediate = 'C'> = Helper<N, [], From, To, Intermediate>
+export type Helper<
+    N extends number,
+    C extends any[],
+    From extends unknown, 
+    To extends unknown, 
+    Intermediate extends unknown
+> = C['length'] extends N ? []
+    : [
+        ...Helper<N, [...C, 1], From, Intermediate, To>,
+        [From, To], 
+        ...Helper<N, [...C, 1], Intermediate, To, From>
+    ]
