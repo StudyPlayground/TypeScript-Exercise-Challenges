0ef640dfd81be34d260270aac637c6a656e1eafe
Kyungseok Lee <leeseok8347@gmail.com>
2024-05-14 20:01:15 +0900

    29785-medium-deep-omit

--- medium/29785-medium-deep-omit/cory.ts
+++ medium/29785-medium-deep-omit/cory.ts
@@ -1,1 +1,8 @@
-// 답을 적어주세요.
+export type Intersection<T> = { [K in keyof T]: T[K] };
+export type DeepOmit<T extends object, U extends string> = U extends `${infer Prefix}.${infer Suffix}`
+    ? Prefix extends keyof T
+        ? Intersection<
+            Omit<T, Prefix> & { [K in Prefix]: T[K] extends object ? DeepOmit<T[K], Suffix> : T[K] }
+        >
+        : T
+    : Omit<T, U>
